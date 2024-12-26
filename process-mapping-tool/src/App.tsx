cb1ff4cb89fe736febc640e2b0d5b6a5d00a09d7
Goh Shu Jie <sgoh0039@student.monash.edu>
2024-12-26 08:44:29 +0800

    Added resizable panel

--- process-mapping-tool/src/App.tsx
+++ process-mapping-tool/src/App.tsx
@@ -1,8 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from "./components/NavBar";
+import NavBar from "./components/NavBar.tsx";
+import ResizableBar from "./components/ResizableBar.tsx";
 
 function App() {
-  return (<div><NavBar></NavBar></div>);
+  return (
+    <div>
+      <NavBar />
+      <div style={{ display: 'flex', height: 'calc(100vh - 56px)' }}>
+        <ResizableBar />
+        <div style={{ flex: 1, padding: '20px' }}>
+          <h1>Welcome to Rose Playbook</h1>
+          <p>This is the main content area.</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
