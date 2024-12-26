df749f5f4624df0cd7b54e3eb7196111b900c728
Goh Shu Jie <sgoh0039@student.monash.edu>
2024-12-26 22:52:53 +0800

    Added a section bar for users to chose diagramming tools

--- process-mapping-tool/src/App.tsx
+++ process-mapping-tool/src/App.tsx
@@ -1,13 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from "./components/NavBar.tsx";
-import ResizableBar from "./components/ResizableBar.tsx";
+import NavBar from "./components/NavBar";
+import SectionBar from "./components/SectionBar";
+import ResizableBar from "./components/ResizableBar";
 
 function App() {
   return (
     <div>
+      {/* Navigation Bar */}
       <NavBar />
-      <div style={{ display: 'flex', height: 'calc(100vh - 56px)' }}>
+      
+      {/* Section Bar */}
+      <SectionBar />
+      
+      {/* Main Layout with Resizable Bar */}
+      <div style={{ display: 'flex', height: 'calc(100vh - 112px)' }}>
+        {/* Resizable Sidebar */}
         <ResizableBar />
+        
+        {/* Main Content */}
         <div style={{ flex: 1, padding: '20px' }}>
           <h1>Welcome to Rose Playbook</h1>
           <p>This is the main content area.</p>
