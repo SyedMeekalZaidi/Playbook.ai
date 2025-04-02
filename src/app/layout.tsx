8506e32daa4c44aef9eb6d36f2148bc4bfe3c6e8
Goh Shu Jie <sgoh0039@student.monash.edu>
2025-04-02 09:20:18 +0800

    Implemented bpmn diagram

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,6 @@
 // src/app/layout.tsx
 import type { Metadata } from "next";
-import "../styles/globals.css"; // Your Tailwind styles
-// Load BPMN core styles globally (these are required for the modeler to render)
-// import "/bpmn-modeler/assets/bpmn-js/diagram-js.css";
-// import "/bpmn-modeler/assets/bpmn-js/bpmn-js.css";
-// import "/bpmn-modeler/assets/bpmn-js/bpmn-font/css/bpmn-embedded.css";
+import "../styles/globals.css";
 
 export const metadata: Metadata = {
   title: "Process Mapping Tool",
